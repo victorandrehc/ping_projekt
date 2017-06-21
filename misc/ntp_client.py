20a10253fb11045b2c955c98be850a033927c11f
Victor Costa <victorandrehc@gmail.com>
2017-06-21 18:15:05 +0200

    ?

---
+++ misc/ntp_client.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from contextlib import closing
+from socket import socket, AF_INET, SOCK_DGRAM
+import sys
+import struct
+import time
+
+NTP_PACKET_FORMAT = "!12I"
+NTP_DELTA = 2208988800L # 1970-01-01 00:00:00
+NTP_QUERY = '\x1b' + 47 * '\0'  
+
+def ntp_time(host="pool.ntp.org", port=123):
+        with closing(socket( AF_INET, SOCK_DGRAM)) as s:
+            s.sendto(NTP_QUERY, (host, port))
+            msg, address = s.recvfrom(1024)
+        unpacked = struct.unpack(NTP_PACKET_FORMAT,
+                       msg[0:struct.calcsize(NTP_PACKET_FORMAT)])
+        return unpacked[10] + float(unpacked[11]) / 2**32 - NTP_DELTA
+
+
+if __name__ == "__main__":
+    print time.ctime(ntp_time()).replace("  "," ")
