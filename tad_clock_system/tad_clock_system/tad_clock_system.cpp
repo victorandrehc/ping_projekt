1eb7eaeb61e37ddc262dc4191065b722503733cf
Victor Costa <victorandrehc@gmail.com>
2017-06-27 14:27:43 +0200

    aaa

--- tad_clock_system/tad_clock_system/tad_clock_system.cpp
+++
@@ -1,6 +0,0 @@
-#include <Arduino.h>
-#include "tad_clock_system.h"
-
-void function(){
-	Serial.println("Hello Waorld");
-}
--- tad_clock_system/tad_clock_system/tad_clock_system.h
+++
@@ -1,9 +0,0 @@
-#ifndef TAD_CLOCK_SYSTEM_H
-#define TAD_CLOCK_SYSTEM_H
-
-#include <Arduino.h>
-
-void function();
-
-
-#endif
