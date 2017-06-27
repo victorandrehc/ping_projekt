97c2f4d5c06d9fbdb06f83024d2f7f7b4050d5a6
Victor Costa <victorandrehc@gmail.com>
2017-06-27 17:26:01 +0200

    ?

--- clock_system/clock_system.ino
+++ clock_system/clock_system.ino
@@ -3,7 +3,16 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.print("\n");
-
+  TimeStack stack;
+  Time t1;
+  t1.timestamp=millis();
+  stack.insert(t1);
+  stack.print();
+  delay(500);
+  Time t2;
+  t2.timestamp=millis();
+  stack.insert(t2);
+  stack.print();
 }
 
 void loop() {
