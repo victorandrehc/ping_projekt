339eb0ec18daff0c11331ab934db2f7c837699d3
Victor Costa <victorandrehc@gmail.com>
2017-07-05 17:40:54 +0200

    inserting file example

---
+++ misc/card/card.ino
@@ -0,0 +1,48 @@
+#include <SPI.h>
+#include <SD.h>
+
+File root;
+
+void printDirectory(File dir, int numTabs);
+
+
+void setup(){
+  Serial.begin(9600);
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+  root=SD.open("/");
+  //printDirectory(root, 0);
+
+  File myFile=getFile(root);
+  
+  while (myFile.available()) {
+    Serial.print((char)myFile.read());
+  }
+
+}
+
+void loop(){
+
+}
+
+File getFile(File dir){
+  //returns object from the first file on the SD card
+  File entry;
+  entry=dir.openNextFile();
+  if(!entry){
+    Serial.println("Error");
+    while(true);
+  }else if(entry.isDirectory()){
+    return getFile(entry);
+  }else{
+    Serial.println(entry.name());
+    return SD.open(entry.name());
+  }
+}
+
+
